0b7723b07cf8c7a76a69aa65f69f2ef3a86d08d3
Natallia Stelmakh <stnata.coding@gmail.com>
2020-07-16 11:05:11 -0400

    started js file

---
+++ script.js
@@ -0,0 +1,5 @@
+//create a var to save result "cityname" from a search
+//create a key to a pet finder
+//create a key to a truewayplaces
+//make an ajax request with petfinder to bring out locations of organizations
+///make an ajax request to get park location and pet stores
