3c96ff8b426a33d4bd52925ce270042493ae070b
Natallia Stelmakh <stnata.coding@gmail.com>
2020-07-16 16:47:53 -0400

    Working api request for petfinder

--- script.js
+++ script.js
@@ -2,33 +2,24 @@
 //create a key to a pet finder
 //create a key to a truewayplaces
 //make an ajax request with petfinder to bring out locations of organizations
-///make an ajax request to get park location and pet stores
-var cityname=""
 
-// $("#search").on("click", function (event){
-//     event.preventDefault();
-    
-//   var cityname = $("#townSearch").val().trim();
-//   $("#currentCity").empty()
+var pf = new petfinder.Client({apiKey: "vYNkq3wbvswUKkr81aYrKbMyaGgd2JHx8S47lGH37GfkUoqgtm", secret: "tXoLVsBVrwoO8foqw23nd1AcDfLyI4EJNYVnrxE9"});
 
-  
-    // var queryURL ="https://www.petfinder.com/member/us/nj/jersey-city/nj333-petfinder-test-account/?referrer_id=d7e3700b-2e07-11e9-b3f3-0800275f82b1"
-    // 
-    var queryURL ="https://api.petfinder.com/v2/organizations"
-     $.ajax({
-  url: queryURL,
-  method: "GET"
-   }).then(function(response) {
-       console.log(response)
-   })
+pf.animal.search()
+    .then(function (response) {
+        console.log(response)
+        // Do something with `response.data.animals`
+    })
+    .catch(function (error) {
+        // Handle the error
+    });
+
+pf.organization.search({location: "Minneapolis, MN"})
+  .then(resp => {
+      console.log(resp)
+    // Do something with resp.data.organizations
+  });
 
 
-//    var queryURL = ""
-//   $.ajax({
-//   url: queryURL,
-//   method: "GET"
-//    }).then(function(response) {
-//        console.log(response)
-//    })
-// })
-//
+
+
