31dfa19fc53e8c0980fe1f117d73ba0f7ceba921
kao.ring <kao.ring@yahoo.com>
2020-07-19 22:20:32 -0400

    final check (KC)

--- Assets/script.js
+++ Assets/script.js
@@ -7,9 +7,19 @@
 var userInput;
 var parkArray = [];
 var storeArray = [];
-var imgList = ["images/image0.jpg","images/image1.jpg","images/image2.jpg","images/image3.jpg","images/image4.jpg","images/image5.jpg","images/image6.jpg","images/image7.jpg","images/image8.jpg","images/image9.jpg","images/image10.jpg"];
-
-
+var imgList = [
+  "images/image0.jpg",
+  "images/image1.jpg",
+  "images/image2.jpg",
+  "images/image3.jpg",
+  "images/image4.jpg",
+  "images/image5.jpg",
+  "images/image6.jpg",
+  "images/image7.jpg",
+  "images/image8.jpg",
+  "images/image9.jpg",
+  "images/image10.jpg",
+];
 
 $(function () {
   //get city name and generate all cards when reloaded
@@ -37,9 +47,8 @@ $("form").submit(function (event) {
 
   findOrganization();
   findPark();
-  // findStore();
+  findStore();
 });
-
 
 // //=======================ADD KEY "ENTER" AS A TRIGGER
 // $("input").on("keypress", function (event) {
@@ -65,13 +74,15 @@ function findOrganization() {
     console.log(listLength);
 
     for (var i = 0; i < listLength; i++) {
-      var newAdoptionOrgCard = $('<div class="card">')
-      var newAdoptionCardContent =$('<div class="card-content">')
-      var media = $("<div class='media'>")
-         
-      var dogImg = $('<div class="media-left"><figure class="image is-96x96"><img id=dog>')
+      var newAdoptionOrgCard = $('<div class="card">');
+      var newAdoptionCardContent = $('<div class="card-content">');
+      var media = $("<div class='media'>");
+
+      var dogImg = $(
+        '<div class="media-left"><figure class="image is-96x96"><img id=dog>'
+      );
       var imageUrl = imgList[i];
-      console.log(imageUrl)
+      console.log(imageUrl);
       $(`#dog`).attr("src", imageUrl);
       $("img").attr("alt", "dog image");
 
@@ -95,32 +106,31 @@ function findOrganization() {
         address.push(postcode);
       }
       console.log(address);
-      
-      var mediaContent = $("<div class='media-content'>")
-      var name = $("<h2 class='title is-4 park-1'>").text(resp.data.organizations[i].name);
-      $(mediaContent).append(name); 
-      
 
+      var mediaContent = $("<div class='media-content'>");
+      var name = $("<h2 class='title is-4 park-1'>").text(
+        resp.data.organizations[i].name
+      );
+      $(mediaContent).append(name);
 
       var phone = $('<h3 class="park-phone-1">').text(
         resp.data.organizations[i].phone
       );
       if (phone !== null || phone !== "") {
-      $(mediaContent).append(phone);
+        $(mediaContent).append(phone);
       }
       var email = $('</h3><h3 class="park-link-1">').text(
         resp.data.organizations[i].email
       );
       if (email !== null || email !== "") {
-      $(mediaContent).append(email);
+        $(mediaContent).append(email);
       }
       var addressInfo = $("<h3 class='park-address-1'>").text(address);
       $(mediaContent).append(addressInfo);
-      $(media).append(mediaContent)
-      $(newAdoptionCardContent).append(media)
-      $(newAdoptionOrgCard).append(newAdoptionCardContent)
+      $(media).append(mediaContent);
+      $(newAdoptionCardContent).append(media);
+      $(newAdoptionOrgCard).append(newAdoptionCardContent);
       $("#adoptionCards").append(newAdoptionOrgCard);
-
     }
   });
 }
